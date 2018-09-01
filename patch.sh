01269ec353555180c6ad8229808df9638afc6aa6
FoxD <foxrtb@gmail.com>
2018-09-01 14:13:58 +0300

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -4,7 +4,7 @@
 #
 cat <<EOF > /etc/cron.hourly/restart-mn.sh
 #!/bin/bash
-/usr/local/binnodos-cli clearbanned 
+/usr/local/bin/anodos-cli clearbanned 
 /usr/local/bin/anodos-cli stop 
 /usr/local/bin/anodosd -reindex
 EOF
