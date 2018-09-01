a66d7aaddf32203e0ec7bb0fb900a80cd2ac785d
FoxD <foxrtb@gmail.com>
2018-09-01 14:12:24 +0300

    added some comments

--- patch.sh
+++ patch.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+#
+#restarts the daemon every hour
+#
 cat <<EOF > /etc/cron.hourly/restart-mn.sh
 #!/bin/bash
 /usr/local/binnodos-cli clearbanned 
 /usr/local/bin/anodos-cli stop 
 /usr/local/bin/anodosd -reindex
 EOF
+
+#make it +x
 /bin/chmod +x /etc/cron.hourly/restart-mn.sh
